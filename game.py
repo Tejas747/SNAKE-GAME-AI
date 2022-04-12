1cb779f863bc5563fb093d598412c297361192e1
Tejas747 <59719068+Tejas747@users.noreply.github.com>
2022-04-12 15:00:34 +0530

    Update game.py

--- game.py
+++ game.py
@@ -19,8 +19,8 @@ Point = namedtuple('Point', 'x, y')
 # rgb colors
 WHITE = (255, 255, 255)
 RED = (200,0,0)
-BLUE1 = (0, 0, 255)
-BLUE2 = (0, 100, 255)
+BLUE1 = (225, 225, 0)
+BLUE2 = (225, 100, 225)
 BLACK = (0,0,0)
 
 BLOCK_SIZE = 20
